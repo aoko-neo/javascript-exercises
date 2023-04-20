56e28112c25fc93900c04775131831745a769131
aoko-faith <97647953+aoko-neo@users.noreply.github.com>
2023-04-20 09:35:35 +0300

    remove a single value from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...value) {
+     let array2 = [];
+     for (let i = 0; i < array.length; i++) {
+          if (array[i] !==value) {
+               array2.push(array[i])
+          }
+     }
+     return array2;
 };
 
 // Do not edit below this line
