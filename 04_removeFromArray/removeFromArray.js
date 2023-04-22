9a3e76e111100b198c9850c6a0be2102f59649af
aoko-faith <97647953+aoko-neo@users.noreply.github.com>
2023-04-23 00:27:01 +0300

    remove values from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,5 @@
-const removeFromArray = function(array, ...value) {
-     let array2 = [];
-     for (let i = 0; i < array.length; i++) {
-          if (array[i] !==value) {
-               array2.push(array[i])
-          }
-     }
-     return array2;
+const removeFromArray = function(array, ...args) {
+     return array.filter(i => !args.includes(i))
 };
 
 // Do not edit below this line
